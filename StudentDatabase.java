db31589f609170b2b1ae532e48b683512c9d5b8e
Alexis Low <alexisthelow@gmail.com>
2017-08-15 19:38:07 -0600

    lol

--- StudentDatabase.java
+++ StudentDatabase.java
@@ -7,7 +7,7 @@ class Student {
 public class StudentDatabase {
 	public static void main(String[] args) {
 		Student[] db;
-		db = new Student[3];
+		db = new Student[4];
 		
 		db[0] = new Student();
 		db[0].name = "Esteban";
@@ -16,13 +16,18 @@ public class StudentDatabase {
 		
 		db[1] = new Student();
 		db[1].name = "Dave";
-		db[1].credits = 15;
-		db[1].gpa = 4.0;
+		db[1].credits = 2;
+		db[1].gpa = 0.4;
 		
 		db[2] = new Student();
 		db[2].name = "Michelle";
 		db[2].credits = 132;
 		db[2].gpa = 3.72;
+		
+		db[3] = new Student();
+		db[3].name = "Alexis 'Power Attack' Low";
+		db[3].credits = 80085;
+		db[3].gpa = 99.98;
 		
 		for (int i = 0; i < db.length; i++) {
 			System.out.println("Name: " + db[i].name);
@@ -31,12 +36,19 @@ public class StudentDatabase {
 		}
 		
 		int maxLoc = 0;
+		int minLoc = 0;
 		
 		for (int i = 1; i < db.length; i++) {
 			if (db[i].gpa > db[maxLoc].gpa)
 				maxLoc = i;
 		}
 		
+		for (int i = 1; i < db.length; i++) {
+			if (db[i].gpa < db[minLoc].gpa)
+				minLoc = i;
+		}
+		
 		System.out.println(db[maxLoc].name + " has the highest GPA.");
+		System.out.println(db[minLoc].name + " has the lowest GPA, and needs to go to clown school.");
 	}
 }
